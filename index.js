c794707dbc34ccff610730648a735b8802a56436
lieby weintraub <liebahinda@gmail.com>
2019-09-17 05:29:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function addNewElementAsLi() {
 }
 
 function clearEmployeeListOnLinkClick() {
-  const clearLink = document.querySelector('a')
-  clearLink.addEventListener('click',
+  const clearList = document.querySelector('a')
+  clearList.addEventListener('click',
     function(clickEvent) {
       let ul = document.querySelector('ul')
       ul.innerHTML = ''
